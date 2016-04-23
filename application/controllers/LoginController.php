88f25f97eca0af1711c952c49b11a5f824206a4f
Alisson Chiquitto <chiquitto@gmail.com>
2016-04-23 17:52:06 -0300

    Preparando material

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -3,7 +3,16 @@
 class LoginController extends Blog_Controller_Action {
 
     public function indexAction() {
+        $form = new Application_Form_Login();
         
+        if ($this->getRequest()->isPost()) {
+            $values = $this->getAllParams();
+            if ($form->isValid($values)) {
+                // autenticacao do usuario
+            }
+        }
+        
+        $this->view->form = $form;
     }
 
     public function logoutAction() {
